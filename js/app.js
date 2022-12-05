cabe60787e4fe6aa82f02bc3f9948163053fbf03
Soaen <sylvain.vansteenkiste59@gmail.com>
2022-12-05 14:26:43 +0100

    Menu Burger done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
   $(".burger").click(function () {
     $(".menunav").toggleClass("open");
+    $(".burger").toggleClass("hidden");
+    $(".cross").toggleClass("hidden");
+  });
+});
+
+$(document).ready(function () {
+  $(".cross").click(function () {
+    $(".menunav").toggleClass("open");
+    $(".burger").toggleClass("hidden");
+    $(".cross").toggleClass("hidden");
   });
 });
